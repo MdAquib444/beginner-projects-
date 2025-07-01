295931aec318806517f2e3bf03b0dbeb255c1094
Md Aquib <88885515+MdAquib444@users.noreply.github.com>
2025-07-01 09:08:27 +0530

    update basic-webapp-setup/js/main.js

--- basic-webapp-setup/js/main.js
+++ basic-webapp-setup/js/main.js
@@ -1,10 +1,12 @@
 let paragraph = document.querySelector("p");
 
 window.onload = () => {
+  const randomDelay = Math.random() * (2000 - 500) + 500; // 500ms to 2000ms
+
   setTimeout(() => {
     paragraph.innerHTML = "JavaScript Setup Successful.";
     paragraph.style.transition = "all 0.5s ease";
     paragraph.style.transform = "translateY(30px)";
     paragraph.style.color = "#15821c";
-  }, 2000);
+  }, randomDelay);
 };
