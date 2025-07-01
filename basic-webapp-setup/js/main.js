7ab7bf9ff7c281b12217830b17ee0289bd2dc754
Md Aquib <88885515+MdAquib444@users.noreply.github.com>
2025-07-01 09:05:02 +0530

    update basic-webapp-setup/js/main.js

--- basic-webapp-setup/js/main.js
+++ basic-webapp-setup/js/main.js
@@ -1,5 +1,5 @@
 let paragraph = document.querySelector("p");
 window.onload = ()=>{
   paragraph.innerHTML = "JavaScript Setup Successful.";
-  paragraph.style.color = "linear-gradient(to right bottom, #15821c, #056cf8)";
+  paragraph.style.color = "#15821c";
 };
