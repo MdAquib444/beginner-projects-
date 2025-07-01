f811d9f66ded69067a1ec0fb962b71dae4bbdbc6
Md Aquib <88885515+MdAquib444@users.noreply.github.com>
2025-07-01 09:06:52 +0530

    update basic-webapp-setup/js/main.js

--- basic-webapp-setup/js/main.js
+++ basic-webapp-setup/js/main.js
@@ -1,5 +1,10 @@
 let paragraph = document.querySelector("p");
-window.onload = ()=>{
-  paragraph.innerHTML = "JavaScript Setup Successful.";
-  paragraph.style.color = "#15821c";
+
+window.onload = () => {
+  setTimeout(() => {
+    paragraph.innerHTML = "JavaScript Setup Successful.";
+    paragraph.style.transition = "all 0.5s ease";
+    paragraph.style.transform = "translateY(30px)";
+    paragraph.style.color = "#15821c";
+  }, 2000);
 };
