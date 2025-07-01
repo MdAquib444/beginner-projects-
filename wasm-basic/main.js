0913c8b7991d424deeac27ea7e5fbe472cccd3b4
Md Aquib <88885515+MdAquib444@users.noreply.github.com>
2025-07-01 14:51:38 +0530

    Delete wasm-basic directory

--- wasm-basic/main.js
+++
@@ -1,7 +0,0 @@
-fetch('hello.wasm')
-  .then(res => res.arrayBuffer())
-  .then(bytes => WebAssembly.instantiate(bytes))
-  .then(result => {
-    const val = result.instance.exports.hello();
-    document.getElementById("output").textContent = "✅ Hello from WASM! Value: " + val;
-  });
